3592cfcb7be5e9f3f05ac3d47efb7a0815d2d9ab
Jung Hyun Park <rootjh937@gmail.com>
2021-05-23 11:32:22 +0900

    project init

---
+++ board/spring-bd/src/test/java/com/board/springbd/SpringBdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.board.springbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
